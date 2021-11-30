464a1a40d475c6fb88070150395b930ce33da04d
Veronica Lino <Veronica@digitalcrafts.com>
2021-11-30 09:50:21 -0600

    updated readme

--- src/components/SearchStudent.jsx
+++ src/components/SearchStudent.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const SearchStudent = () => {
 
     // search for a student by name, if nothing found, return empty array
+
+    // save the search results for the last 3 searches
   return (
     <>
       Search Student
