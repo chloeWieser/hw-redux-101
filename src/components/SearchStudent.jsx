3e2f0e37a7f8d6541e3730ed5cb6ba1bee50126e
chloeWieser <chloerosewieser@gmail.com>
2022-05-05 17:34:45 -0400

    search

--- src/components/SearchStudent.jsx
+++ src/components/SearchStudent.jsx
@@ -1,6 +1,39 @@
-import React from 'react'
+// import React, {useState, useEffect} from 'react'
+
+// const SearchStudent = () => {
+
+//     // search for a student by name, if nothing found, return empty array
+
+//     // save the search results for the last 3 searches
+//   return (
+//     <>
+//       Search Student
+//     </>
+//   )
+// }
+
+// export default SearchStudent
+
+
+import React, {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'; 
 
 const SearchStudent = () => {
+
+  const students = useSelector(state => state.students)
+  const [filteredStudents, setFilteredStudents] = useState([])
+
+  const [filteredText, setFilteredText] = useState("")
+
+  const handleChange = (e) => {
+    setFilteredText(e.target.value)
+
+    let filteredStudents = students.filter(student =>{
+        return student.fName.toLowerCase().includes(filteredText.toLowerCase())
+    })
+
+    setFilteredStudents(filteredStudents)
+}
 
     // search for a student by name, if nothing found, return empty array
 
@@ -8,6 +41,15 @@ const SearchStudent = () => {
   return (
     <>
       Search Student
+      <input type="text" value={filteredText} onChange={handleChange} /> 
+      <ul>
+        {filteredStudents.map((student) =>{
+            return <li key={student.id}>
+                    <h2>{student.fName}, {student.city}</h2>
+
+                </li>
+        })}
+    </ul>
     </>
   )
 }
